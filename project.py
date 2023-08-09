fc533241150dca296b3c8afa1a452c24771cfd6e
Sam Howard <essjay89@hotmail.co.uk>
2023-08-09 17:19:25 +0100

    Added cput utilization

--- project.py
+++ project.py
@@ -11,7 +11,6 @@ zone = 'europe-west2-c'
 
 # Call the API to list instances in the specified zone 
 instances = compute_client.list(project=project_id, zone=zone)
-instances_list = list(compute_client.list(project=project_id, zone=zone))
 ##########################################################################
 
 
@@ -20,6 +19,10 @@ from flask_restful import Resource, Api
 from flask_cors import CORS 
 from flask_httpauth import HTTPBasicAuth
 import bcrypt
+import time
+from google.cloud import monitoring_v3
+from datetime import datetime
+
 
 app = Flask("GCPInstancesAPI")
 api = Api(app)
@@ -27,24 +30,46 @@ CORS(app)
 auth = HTTPBasicAuth()
 
 
-# Define your username and password
+
 USERNAME = "admin"
 PASSWORD = "$2b$12$lyR1usJNQWDo6ciYe/NBoO8co2urbyK4OHK7jhlVNqRPyCM9v5cyW"
 
-# Basic authenticati aon callback
+
 @auth.verify_password
 def verify_password(username, password):
     return username == USERNAME and bcrypt.checkpw(password.encode('utf-8'), PASSWORD.encode('utf-8'))
+
+
+
+
+def cpu_utilization(project_id, zone, instance_id):
+    client = monitoring_v3.MetricServiceClient()
+    metric_type = "compute.googleapis.com/instance/cpu/utilization"
+    resource_name = f"projects/{project_id}/zones/{zone}/instances/{instance_id}"
+    interval = monitoring_v3.TimeInterval({"end_time": {"seconds": int(time.time())}, "start_time": {"seconds": int(time.time() - 3600)}})
+
+    metric_results = client.list_time_series(name=f"projects/{project_id}", filter=f'resource.type="gce_instance" AND resource.label.instance_id="{instance_id}" AND metric.type="{metric_type}"', interval=interval, view=monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL)
+
+    for result in metric_results:
+        latest_point = result.points[-1]
+        utilization = latest_point.value.double_value
+        timestamp_seconds = latest_point.interval.end_time.timestamp_pb().seconds + latest_point.interval.end_time.timestamp_pb().nanos / 1e9
+        timestamp = datetime.fromtimestamp(timestamp_seconds).strftime('%Y-%m-%d %H:%M:%S')
+        return(f"{utilization:.2f}%") 
+
 
 
 VMs = {
 }
 
 
+
 for instance in instances:
     VMs[instance.name] = {
         'Status': instance.status,
-        'External IP': instance.network_interfaces[0].access_configs[0].nat_i_p
+        'External IP': instance.network_interfaces[0].access_configs[0].nat_i_p,
+        'Instance ID': instance.id,
+        'CPU Utilization': cpu_utilization(project_id, zone, instance.id)
         }
 
 class VM(Resource):
